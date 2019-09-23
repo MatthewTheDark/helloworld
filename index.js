ca8c4ab5a944f774cb4933c11102edbf09e52c1f
moravecmatous <moravecmatous@porg.cz>
2019-09-23 17:22:41 +0200

    Pocita pozadavky na muj skvely web

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
+let citac = 0;
+
 http.createServer((req, res) => {
+    if (req.url == "/"){ //url - co po nas prohlizec chce
+        citac++;
+    }
     res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>helloworld v2.0</body></html>");
+    res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>Počet: " +citac +"</body></html>");
 }). listen(8888);
