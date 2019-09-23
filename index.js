12f564d294e8ce451b0b680ea395c06479536a4a
moravecmatous <moravecmatous@porg.cz>
2019-09-23 17:38:08 +0200

    jsondemo

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ http.createServer((req, res) => {
     if (req.url == "/jinastranka"){
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>jina stranka</body></html>");
+    } else if (req.url == "/jsondemo"){
+        res.writeHead(200, {"Content-type": "application/json"});
+        let obj = {};
+        obj.jmeno = "Link";
+        obj.prijmeni = "Smith";
+        obj.rokNarozeni = 1980;
+        JSON.stringify(obj);
+        res.end(JSON.stringify(obj));
+
     } else {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>Počet: " +citac +"</body></html>");
