ddcabe1cf18674a80fbebd58fb8c604925c137b2
moravecmatous <moravecmatous@porg.cz>
2019-09-23 17:28:11 +0200

    vic stranek

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ http.createServer((req, res) => {
     if (req.url == "/"){ //url - co po nas prohlizec chce
         citac++;
     }
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>Počet: " +citac +"</body></html>");
+    if (req.url == "/jinastranka"){
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>jina stranka</body></html>");
+    } else {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF-8'></head><body>Počet: " +citac +"</body></html>");
+    }
+
 }). listen(8888);
